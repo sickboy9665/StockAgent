5724a28b53c3392c5c7d0b3a1e90c7a5513b8780
Abhishek <shinganabhishek@gmail.com>
2025-03-12 23:29:40 +0530

    Modified code for agno compatibility

--- finance_agent.py
+++ finance_agent.py
@@ -1,72 +1,87 @@
+
+# from phi.agent import Agent
+# from phi.model.google import Gemini
+# from phi.tools.yfinance import YFinanceTools
+# from phi.tools.duckduckgo import DuckDuckGo
+
+
+
+
+# ## web search agent
+# web_search_agent=Agent(
+#     name="Web Search Agent",
+#     role="Search the web for the information",
+#     model=Gemini(id="gemini-1.5-flash",api_key=geminikey),
+#     tools=[DuckDuckGo()],
+#     instructions=["Alway include sources"],
+#     show_tools_calls=True,
+#     markdown=True,
+
+# )
+
+# ## Financial agent
+# finance_agent=Agent(
+#     name="Finance AI Agent",
+#     model=Gemini(id="gemini-1.5-flash", api_key=geminikey),
+#     tools=[
+#         YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,
+#                       company_news=True),
+#     ],
+#     instructions=["Use tables to display the data"],
+#     show_tool_calls=True,
+#     markdown=True,
+
+# )
+
+# multi_ai_agent=Agent(
+#     team=[web_search_agent,finance_agent],
+#     instructions=["Always include sources","Use table to display the data"],
+#     model = Gemini(id="gemini-1.5-flash", api_key=geminikey),
+#     show_tool_calls=True,
+#     markdown=True,
+# )
+
+# multi_ai_agent.print_response("Summarize analyst recommendation and share the latest news for NVDA",stream=True,
+#                              show_full_reasoning=True )
+
+
+from agno.agent import Agent
+# from agno.models.openai import OpenAIChat
+from agno.tools.duckduckgo import DuckDuckGoTools
+from agno.tools.yfinance import YFinanceTools
+from agno.models.google import Gemini
 import os
 from dotenv import load_dotenv
 load_dotenv()
 geminikey = os.getenv("GOOGLE_GEMINI_KEY")
-from phi.agent import Agent
-from phi.model.google import Gemini
-from phi.tools.yfinance import YFinanceTools
-from phi.tools.duckduckgo import DuckDuckGo
 
-
-
-
-## web search agent
-web_search_agent=Agent(
-    name="Web Search Agent",
-    role="Search the web for the information",
-    model=Gemini(id="gemini-1.5-flash",api_key=geminikey),
-    tools=[DuckDuckGo()],
-    instructions=["Alway include sources"],
-    show_tools_calls=True,
-    markdown=True,
-
-)
-
-## Financial agent
-finance_agent=Agent(
-    name="Finance AI Agent",
-    model=Gemini(id="gemini-1.5-flash", api_key=geminikey),
-    tools=[
-        YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,
-                      company_news=True),
-    ],
-    instructions=["Use tables to display the data"],
-    show_tool_calls=True,
-    markdown=True,
-
-)
-
-multi_ai_agent=Agent(
-    team=[web_search_agent,finance_agent],
-    instructions=["Always include sources","Use table to display the data"],
-    model = Gemini(id="gemini-1.5-flash", api_key=geminikey),
+web_agent = Agent(
+    name="Web Agent",
+    role="Search the web for information",
+    model=Gemini(id="gemini-2.0-flash-exp",api_key=geminikey),
+    tools=[DuckDuckGoTools()],
+    instructions="Always include sources",
     show_tool_calls=True,
     markdown=True,
 )
 
-multi_ai_agent.print_response("Summarize analyst recommendation and share the latest news for NVDA",stream=True)
+finance_agent = Agent(
+    name="Finance Agent",
+    role="Get financial data",
+    model=Gemini(id="gemini-2.0-flash-exp",api_key=geminikey),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True)],
+    instructions="Use tables to display data",
+    show_tool_calls=True,
+    markdown=True,
+)
 
+agent_team = Agent(
+    team=[web_agent, finance_agent],
+    model=Gemini(id="gemini-2.0-flash-exp",api_key=geminikey),
+    instructions=["Always include sources", "Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
+)
 
-
-
-
-## SAMPLE TESTING GEMINI RESPONSE
-
-# from dotenv import load_dotenv
-
-# load_dotenv()
-# import os
-# from phi.agent import Agent, RunResponse
-# from phi.model.google import Gemini
-# geminikey = os.getenv("GOOGLE_GEMINI_KEY")
-# agent = Agent(
-#     model=Gemini(id="gemini-1.5-flash", api_key=geminikey),
-#     markdown=True,
-# )
-
-# # Get the response in a variable
-# # run: RunResponse = agent.run("Share a 2 sentence horror story.")
-# # print(run.content)
-
-# # Print the response in the terminal
-# agent.print_response("Share a 2 sentence horror story.")
+agent_team.print_response("Summarize analyst recommendation and share the latest news for NVDA and also give me current stock price for the same.",stream=True,
+                          show_full_reasoning=True)
--- playground.py
+++ playground.py
@@ -1,35 +1,35 @@
 
-from phi.agent import Agent
-import phi.api
-from phi.tools.yfinance import YFinanceTools
-from phi.tools.duckduckgo import DuckDuckGo
+from agno.agent import Agent
+import agno.api
+from agno.tools.yfinance import YFinanceTools
+from agno.tools.duckduckgo import DuckDuckGoTools
 from dotenv import load_dotenv
-from phi.model.groq import Groq
-
+from agno.models.groq import Groq
+from agno.storage.agent.sqlite import SqliteAgentStorage
 import os
-import phi
-from phi.playground import Playground, serve_playground_app
+import agno
+from agno.playground import Playground, serve_playground_app
 # Load environment variables from .env file
 load_dotenv()
 groqkey = os.getenv("GROQ_API_KEY")
-phi.api=os.getenv("PHI_API_KEY")
-
+agno.api=os.getenv("AGNO_API_KEY")
+agent_storage: str = "tmp/agents.db"
 ## web search agent
 web_search_agent=Agent(
     name="Web Search Agent",
     role="Search the web for the information",
-    model=Groq(id="llama3-groq-70b-8192-tool-use-preview",api_key=groqkey),
-    tools=[DuckDuckGo()],
+    model=Groq(id="deepseek-r1-distill-llama-70b",api_key=groqkey),
+    tools=[DuckDuckGoTools()],
     instructions=["Alway include sources"],
-    show_tools_calls=True,
+    show_tool_calls=True,
     markdown=True,
-
+    storage=SqliteAgentStorage(table_name="web_agent", db_file=agent_storage),
 )
 
 ## Financial agent
 finance_agent=Agent(
     name="Finance AI Agent",
-    model=Groq(id="llama3-groq-70b-8192-tool-use-preview",api_key=groqkey),
+    model=Groq(id="deepseek-r1-distill-llama-70b",api_key=groqkey),
     tools=[
         YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,
                       company_news=True),
@@ -37,7 +37,7 @@ finance_agent=Agent(
     instructions=["Use tables to display the data"],
     show_tool_calls=True,
     markdown=True,
-
+    storage=SqliteAgentStorage(table_name="finance_agent", db_file=agent_storage),
 )
 
 app=Playground(agents=[finance_agent,web_search_agent]).get_app()
